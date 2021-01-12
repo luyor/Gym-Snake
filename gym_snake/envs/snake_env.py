c1c86d1bbe34362ab81a9bf1439a2f42142b89c4
yuhel <yuhe_lu@sfu.ca>
2021-01-12 22:25:06 +0800

    config observation space

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -1,19 +1,26 @@
-import os, subprocess, time, signal
+import os
+import subprocess
+import time
+import signal
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 from gym_snake.envs.snake import Controller, Discrete
+from gym import spaces
+import numpy as np
 
 try:
     import matplotlib.pyplot as plt
     import matplotlib
 except ImportError as e:
-    raise error.DependencyNotInstalled("{}. (HINT: see matplotlib documentation for installation https://matplotlib.org/faq/installing_faq.html#installation".format(e))
+    raise error.DependencyNotInstalled(
+        "{}. (HINT: see matplotlib documentation for installation https://matplotlib.org/faq/installing_faq.html#installation".format(e))
+
 
 class SnakeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, grid_size=[15,15], unit_size=10, unit_gap=1, snake_size=3, n_snakes=1, n_foods=1, random_init=True):
+    def __init__(self, grid_size=[15, 15], unit_size=10, unit_gap=1, snake_size=3, n_snakes=1, n_foods=1, random_init=True):
         self.grid_size = grid_size
         self.unit_size = unit_size
         self.unit_gap = unit_gap
@@ -21,7 +28,9 @@ class SnakeEnv(gym.Env):
         self.n_snakes = n_snakes
         self.n_foods = n_foods
         self.viewer = None
-        self.action_space = Discrete(4)
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(
+            low=0, high=255, dtype=np.uint8, shape=(self.grid_size[1], self.grid_size[0], 3))
         self.random_init = random_init
 
     def step(self, action):
